103dfd422b1defd07da79235daf5aae6bce32eef
rey256 <medvedosandr@gmail.com>
2024-10-15 10:53:56 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: "/ocw_wallet_page_test/",
-  base: "/ocw_wallet_page_test",
+  // base: "/ocw_wallet_page_test",
   plugins: [vue()],
 });
