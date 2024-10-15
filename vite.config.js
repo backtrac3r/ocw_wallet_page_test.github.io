54e32007d8208e990ccc70b2265387b2006d762e
rey256 <medvedosandr@gmail.com>
2024-10-15 10:48:14 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,4 @@ export default defineConfig({
   base: "/ocw_wallet_page_test",
   publicDir: "/ocw_wallet_page_test/",
   plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
 });
